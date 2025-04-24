279cf192e93fdc047d60379139b17f6a64273370
Hasan Mahmood <hasanmahmudnayeem3027@gmail.com>
2025-04-24 11:19:15 +0600

    Update deep2.py

--- deep2.py
+++ deep2.py
@@ -1,25 +1,7 @@
-# from openai import OpenAI
-
-# client = OpenAI(api_key="sk-7b8f607f94d04583927c5f7c9362ff2e", base_url="https://api.deepseek.com")
-
-# response = client.chat.completions.create(
-#     model="deepseek-chat",
-#     messages=[
-#         {"role": "system", "content": "You are a helpful assistant"},
-#         {"role": "user", "content": "Hello"},
-#     ],
-#     stream=False
-# )
-
-# print(response.choices[0].message.content)
-
-
-
-
 
 import requests
 
-api_key = "sk-7b8f607f94d04583927c5f7c9362ff2e"
+api_key = "API KEY"
 url = "https://api.deepseek.com/v1/chat/completions"
 
 headers = {
